6190f05807ed6ff6e850f0943fbc592656a4f794
CreeperLifeOS <142533646+CreeperLifeOS@users.noreply.github.com>
2023-08-22 19:39:25 +0000

    I SAID BICYCLE BOI

---
+++ Bicycle.java
@@ -0,0 +1,63 @@
+/*
+
+This is a multiline comment... :P
+
+*/
+
+public class Bicycle{
+
+// Put member variables here
+	int wheels;
+	int seats;
+	int gears;
+	boolean brake;
+	int speed;
+
+// Methods can be declared here as well
+	void Move;
+//	void Rotate;
+//	void Accelerate;
+	boolean Brake;
+//	int Gears;
+
+/*
+Methods that only have () are said not to have any parameters
+If you want to throw some info/data to the methods you must
+Allow Methods to accept parameters
+*/
+
+	public Bicycle(){
+		this.wheels = 2;
+		this.seats = 1;
+		this.gears = 2;
+		this.brake = true;
+		this.speed = 0;
+
+	}
+//			(v This below are Parameters v)
+	public Bicycle(int a, int b, int c, boolean haveBrakes){
+		this.wheels = a;
+		this.seats = b;
+		this.gears = c;
+		this.brake = haveBrakes;
+		this.speed = 0;
+
+	}
+
+// void in the following line is the return variable after calling the method
+// void means nothing returns when method is called
+	public void Move(){
+
+		this.speed = 5;
+		
+	}
+
+// Boolean is the return method and when called returns boolean value
+	public boolean Brake(){
+
+		this.speed = 0;
+		return true;
+
+	}
+
+}
---
+++ MainBi.java
@@ -0,0 +1,10 @@
+public class MainBi{
+// All programs must have a main to run code
+// They all have the following declaration
+	public static void main(String[] args){
+		Bicycle A = new Bicycle(2,1,2,false);
+
+		A.Move();
+
+	}
+}
