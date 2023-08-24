4784137708fc447197c345130b35ece25ff77a8e
CreeperLifeOS <142533646+CreeperLifeOS@users.noreply.github.com>
2023-08-24 18:49:52 +0000

    Count from 0 to 0 with me!

---
+++ Count.java
@@ -0,0 +1,13 @@
+public class Count{
+
+	public static void main(String[] args){
+		int count = 0;
+		System.out.println(count);
+		count++;
+		System.out.println(count);
+		System.out.println(++count);
+		System.out.println(count--);
+		System.out.println(--count);
+
+	}
+}
