9028e892a53e1c954d10331d60afc9dc083f9c77
CreeperLifeOS <142533646+CreeperLifeOS@users.noreply.github.com>
2023-08-24 18:34:49 +0000

    Edits for Bicycle

--- Bicycle.java
+++ Bicycle.java
@@ -7,11 +7,12 @@ This is a multiline comment... :P
 public class Bicycle{
 
 // Put member variables here
-	int wheels;
-	int seats;
-	int gears;
-	boolean brake;
-	int speed;
+		int wheels;
+        int seats;
+        boolean hasGears;
+        int gearNumber;
+        boolean brake;
+        int speed;
 
 /*
 Methods that only have () are said not to have any parameters
@@ -22,36 +23,56 @@ Allow Methods to accept parameters
 	public Bicycle(){
 		this.wheels = 2;
 		this.seats = 1;
-		this.gears = 2;
+		this.hasGears = false;
+		this.gearNumber = 0;
 		this.brake = true;
 		this.speed = 0;
 
 	}
-//			(v This below are Parameters v)
-	public Bicycle(int a, int b, int c, boolean haveBrakes){
-		this.wheels = a;
-		this.seats = b;
-		this.gears = c;
-		this.brake = haveBrakes;
+//							(v This below are Parameters v)
+	public Bicycle(boolean hGear, int gNum, boolean haveBrakes){
+        this.wheels = 2;
+    	this.seats = 1;
+	    this.hasGears = hGear;
+        this.gearNumber = gNum;
+        this.brake = haveBrakes;                
 		this.speed = 0;
 
 	}
 
 // void in the following line is the return variable after calling the method
 // void means nothing returns when method is called
-	public void Move(){
+	public void Pedal(){
 
-		this.speed = 5;
-		System.out.println("It works");
+		this.speed = this.speed + 5;
+		System.out.println("Bicycle is moving at " + this.speed + " Units per minute.");
 
 	}
 
 // Boolean is the return method and when called returns boolean value
 	public boolean Brake(){
-
-		this.speed = 0;
-		return true;
+		
+		if(this.brake== false){
+			System.out.println("Bicycle has no Brakes! Slow Down!");
+			return false;
+		}
+        this.speed = 0;
+        System.out.println("Bicycle has stopped.");
+        return true;
 
 	}
+
+// Returns current speed of the bicycle
+	public int getSpeed(){
+            return this.speed;
+    }
+
+    public boolean haveBrakes(){
+            return this.brake;
+    }
+
+    public boolean haveGears(){
+            return this.hasGears;
+    }
 
 }
--- Main.java
+++ Main.java
@@ -4,8 +4,18 @@ public class Main{
 //		Trapezoid A = new Trapezoid(7,2,8);
 //		A.printArea();
 
-		Bicycle A = new Bicycle(2,1,2,false);
-		A.Move();
+		Bicycle A = new Bicycle(true,3,true);
+
+            if(A.haveBrakes() == true){
+                System.out.println("Bicycle has Brakes.");
+            }
+
+            for(int i = 0; i<10; i++){
+                A.Pedal();
+            }
+
+            A.Brake();
+            A.getSpeed();
 
 	}
 	
