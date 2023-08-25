2b060c045130831c6e966b09be344b0fb308fa8d
Omar Suner <142533646+CreeperLifeOS@users.noreply.github.com>
2023-08-25 14:45:52 -0700

    They have numbers now
    DONT FORGET TO CONVERT THE FILE TO .class

--- Modulo.java
+++ Modulo.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class Modulo{
 	public static void main(String[] args){
-		System.out.println("Below we will divide ? by ? and get a remainder of: ");
-		System.out.println(1%1);
-		System.out.println("We'll now divide ? by ? and get a remainder of: ");
-		System.out.println(1%1);
-		System.out.println("Let's try another! Dividing ? by ? gets a remainder of: ");
-		System.out.println(1%1);
-		System.out.println("One more before we bump it up a notch! Dividing ? by ? gets a remainder of: ");
-		System.out.println(1%1);
+		System.out.println("Below we will divide 6 by 4 and get a remainder of: ");
+		System.out.println(6%4);
+		System.out.println("We'll now divide 35 by 8 and get a remainder of: ");
+		System.out.println(35%8);
+		System.out.println("Let's try another! Dividing 60 by 6 gets a remainder of: ");
+		System.out.println(60%6);
+		System.out.println("One more before we bump it up a notch! Dividing 127 by 3 gets a remainder of: ");
+		System.out.println(127%3);
 		System.out.println(" ");
 		System.out.println("Let's try something new!");
 		System.out.println(" ");
