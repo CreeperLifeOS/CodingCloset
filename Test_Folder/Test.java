4d7be3f90a62ffad9f85ac662889407c917fb607
CreeperLifeOS <142533646+CreeperLifeOS@users.noreply.github.com>
2023-08-31 19:55:50 +0000

    NULL 0 RUNTIME ERROR JUMPSCARE!?!?!?!

--- Test_Folder/Test.java
+++ Test_Folder/Test.java
@@ -1,6 +1,19 @@
 public class Test{
 
     public static void main(String[] args){
-        System.out.println("This is the test space!");
+
+        int [] num = {1,5,8,9,10};
+        
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+        System.out.println(num[4]);
+        System.out.println(num);
+
+        String[] stringArray = new String[3];
+        System.out.println(stringArray[0]);
+        int[] num3 = new int[2];
+        System.out.println(num3[0]);
     }
 }
