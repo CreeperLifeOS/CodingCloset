98a594505cb48df99163ea03d7ce46d963b91bfe
CreeperLifeOS <142533646+CreeperLifeOS@users.noreply.github.com>
2023-10-24 19:40:31 +0000

    Smth Idk

--- Test_Folder/SubTest.java
+++ Test_Folder/SubTest.java
@@ -1,11 +1,5 @@
-public class Test{
-
-    public static void main(String[] args){
-        
-
-            System.out.println("posNeg is " + posNeg(1,1,true));
-    }
-    public static boolean posNeg(int a, int b, boolean negative){
+public class SubTest{
+    public boolean posNeg(int a, int b, boolean negative){
         if(a > 0 && b < 0){
             if(negative == true){
                 return false;
